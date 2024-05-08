4f6b8d8f089a96c29e2ba5df36e54d661475c57f
aliahmad <Alijakhar129@gmail.com>
2024-05-09 00:00:20 +0500

    openai integration update response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from custom_utils.Car_Damage import random_name_generator,s3_file_downloader, damage_detection_in_image,damage_detection_in_video
-# from custom_utils.Car_Damage2 import damage_detection_in_image2, damage_detection_in_video2
-# from custom_utils.License_Plate import extract_license_plate_number
+from custom_utils.Car_Damage import random_name_generator,s3_file_downloader, damage_detection_in_image,damage_detection_in_video
+from custom_utils.Car_Damage2 import damage_detection_in_image2, damage_detection_in_video2
+from custom_utils.License_Plate import extract_license_plate_number
 from custom_utils.S3_bucket import upload_file_to_s3_bucket
 from fastapi import FastAPI, HTTPException, Body, Depends
 from fastapi.middleware.cors import CORSMiddleware
@@ -33,67 +33,67 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ################################### Start License plate number extraction endpoint ###########################################
-# @app.post("/license-plate-number-extraction/")
-# async def read_text_from_image(image_data: dict = Body(...), api_token:str=Depends(get_api_token)):
-#     status=False
-#     plate_number=None
-#     detail="successfully"
-#     image_url = image_data.get("image_url")
-#     if not image_url:
-#         # detail="Image URL is missing in the request body"
-#         # return {"status":status,"plateNumber":plate_number,"detail":detail}
-#         raise HTTPException(status_code=400, detail="Image URL is missing in the request body")
-#     try:
-#         status,plate_number,detail = extract_license_plate_number(image_url)
-#         return {"status":status,"plateNumber":plate_number,"detail":detail}
-#     except HTTPException as exc:
-#         detail=exc.detail
-#         return {"status":status,"plateNumber":plate_number,"detail":detail}
+################################### Start License plate number extraction endpoint ###########################################
+@app.post("/license-plate-number-extraction/")
+async def read_text_from_image(image_data: dict = Body(...), api_token:str=Depends(get_api_token)):
+    status=False
+    plate_number=None
+    detail="successfully"
+    image_url = image_data.get("image_url")
+    if not image_url:
+        # detail="Image URL is missing in the request body"
+        # return {"status":status,"plateNumber":plate_number,"detail":detail}
+        raise HTTPException(status_code=400, detail="Image URL is missing in the request body")
+    try:
+        status,plate_number,detail = extract_license_plate_number(image_url)
+        return {"status":status,"plateNumber":plate_number,"detail":detail}
+    except HTTPException as exc:
+        detail=exc.detail
+        return {"status":status,"plateNumber":plate_number,"detail":detail}
     
-# ####################################End License plate number extraction endpoint ###########################################
+####################################End License plate number extraction endpoint ###########################################
 
 
-# @app.post("/damage-detection/")
-# async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
-#     dir_name="s3_files/"
-#     s3_url=body.get("s3_url")
-#     extension=body.get("extension")
-#     file_name=random_name_generator()
+@app.post("/damage-detection/")
+async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
+    dir_name="s3_files/"
+    s3_url=body.get("s3_url")
+    extension=body.get("extension")
+    file_name=random_name_generator()
 
-#     if s3_file_downloader(s3_url,file_name+extension):
-#         processed_file_path=None
-#         if extension.lower()==".jpg":
-#             processed_file_path,message=damage_detection_in_image(dir_name,file_name,extension)
-#         elif extension.lower()==".mp4":
-#             processed_file_path,message=damage_detection_in_video(dir_name,file_name,extension)
+    if s3_file_downloader(s3_url,file_name+extension):
+        processed_file_path=None
+        if extension.lower()==".jpg":
+            processed_file_path,message=damage_detection_in_image(dir_name,file_name,extension)
+        elif extension.lower()==".mp4":
+            processed_file_path,message=damage_detection_in_video(dir_name,file_name,extension)
         
-#         if processed_file_path!=None:
-#             uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-#         return {"processed_file_path": processed_file_path,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
+        if processed_file_path!=None:
+            uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+        return {"processed_file_path": processed_file_path,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
         
 
-# @app.post("/damage-detection2/")
-# async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
-#     dir_name="s3_files/"
-#     s3_url=body.get("s3_url")
-#     extension=body.get("extension")
-#     file_name=random_name_generator()
+@app.post("/damage-detection2/")
+async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
+    dir_name="s3_files/"
+    s3_url=body.get("s3_url")
+    extension=body.get("extension")
+    file_name=random_name_generator()
 
-#     if s3_file_downloader(s3_url,file_name+extension):
-#         processed_file_path=None
-#         if extension.lower()==".jpg":
-#             processed_file_path,message,damage_rectangle,original_img_info=damage_detection_in_image2(dir_name,file_name,extension)
-#             if processed_file_path!=None:
-#                 uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-#             return {"image_s3_link":s3_url,"processed_img_s3_link":uploaded_s3_link,"extension":extension,"message":message,"org_img_info":original_img_info,"damages":damage_rectangle}
+    if s3_file_downloader(s3_url,file_name+extension):
+        processed_file_path=None
+        if extension.lower()==".jpg":
+            processed_file_path,message,damage_rectangle,original_img_info=damage_detection_in_image2(dir_name,file_name,extension)
+            if processed_file_path!=None:
+                uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+            return {"image_s3_link":s3_url,"processed_img_s3_link":uploaded_s3_link,"extension":extension,"message":message,"org_img_info":original_img_info,"damages":damage_rectangle}
         
-#         elif extension.lower()==".mp4":
-#             processed_file_path,message=damage_detection_in_video2(dir_name, file_name ,extension)
+        elif extension.lower()==".mp4":
+            processed_file_path,message=damage_detection_in_video2(dir_name, file_name ,extension)
     
-#             if processed_file_path!=None:
-#                 uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-#             return {"processed_file_path":file_name+extension,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
+            if processed_file_path!=None:
+                uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+            return {"processed_file_path":file_name+extension,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
         
 @app.post("/llm-chat/")
 async def llm_chat_response(chat_request: ChatRequest, api_token:str=Depends(get_api_token)):
