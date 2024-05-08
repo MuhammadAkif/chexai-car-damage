f988ff038bdb91337a6f6eb1b9466a77fe96b24b
aliahmad <Alijakhar129@gmail.com>
2024-05-08 23:58:56 +0500

    openai integration update response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from custom_utils.Car_Damage import random_name_generator,s3_file_downloader, damage_detection_in_image,damage_detection_in_video
-from custom_utils.Car_Damage2 import damage_detection_in_image2, damage_detection_in_video2
-from custom_utils.License_Plate import extract_license_plate_number
+# from custom_utils.Car_Damage import random_name_generator,s3_file_downloader, damage_detection_in_image,damage_detection_in_video
+# from custom_utils.Car_Damage2 import damage_detection_in_image2, damage_detection_in_video2
+# from custom_utils.License_Plate import extract_license_plate_number
 from custom_utils.S3_bucket import upload_file_to_s3_bucket
 from fastapi import FastAPI, HTTPException, Body, Depends
 from fastapi.middleware.cors import CORSMiddleware
@@ -33,67 +33,67 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-################################### Start License plate number extraction endpoint ###########################################
-@app.post("/license-plate-number-extraction/")
-async def read_text_from_image(image_data: dict = Body(...), api_token:str=Depends(get_api_token)):
-    status=False
-    plate_number=None
-    detail="successfully"
-    image_url = image_data.get("image_url")
-    if not image_url:
-        # detail="Image URL is missing in the request body"
-        # return {"status":status,"plateNumber":plate_number,"detail":detail}
-        raise HTTPException(status_code=400, detail="Image URL is missing in the request body")
-    try:
-        status,plate_number,detail = extract_license_plate_number(image_url)
-        return {"status":status,"plateNumber":plate_number,"detail":detail}
-    except HTTPException as exc:
-        detail=exc.detail
-        return {"status":status,"plateNumber":plate_number,"detail":detail}
+# ################################### Start License plate number extraction endpoint ###########################################
+# @app.post("/license-plate-number-extraction/")
+# async def read_text_from_image(image_data: dict = Body(...), api_token:str=Depends(get_api_token)):
+#     status=False
+#     plate_number=None
+#     detail="successfully"
+#     image_url = image_data.get("image_url")
+#     if not image_url:
+#         # detail="Image URL is missing in the request body"
+#         # return {"status":status,"plateNumber":plate_number,"detail":detail}
+#         raise HTTPException(status_code=400, detail="Image URL is missing in the request body")
+#     try:
+#         status,plate_number,detail = extract_license_plate_number(image_url)
+#         return {"status":status,"plateNumber":plate_number,"detail":detail}
+#     except HTTPException as exc:
+#         detail=exc.detail
+#         return {"status":status,"plateNumber":plate_number,"detail":detail}
     
-####################################End License plate number extraction endpoint ###########################################
+# ####################################End License plate number extraction endpoint ###########################################
 
 
-@app.post("/damage-detection/")
-async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
-    dir_name="s3_files/"
-    s3_url=body.get("s3_url")
-    extension=body.get("extension")
-    file_name=random_name_generator()
+# @app.post("/damage-detection/")
+# async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
+#     dir_name="s3_files/"
+#     s3_url=body.get("s3_url")
+#     extension=body.get("extension")
+#     file_name=random_name_generator()
 
-    if s3_file_downloader(s3_url,file_name+extension):
-        processed_file_path=None
-        if extension.lower()==".jpg":
-            processed_file_path,message=damage_detection_in_image(dir_name,file_name,extension)
-        elif extension.lower()==".mp4":
-            processed_file_path,message=damage_detection_in_video(dir_name,file_name,extension)
+#     if s3_file_downloader(s3_url,file_name+extension):
+#         processed_file_path=None
+#         if extension.lower()==".jpg":
+#             processed_file_path,message=damage_detection_in_image(dir_name,file_name,extension)
+#         elif extension.lower()==".mp4":
+#             processed_file_path,message=damage_detection_in_video(dir_name,file_name,extension)
         
-        if processed_file_path!=None:
-            uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-        return {"processed_file_path": processed_file_path,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
+#         if processed_file_path!=None:
+#             uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+#         return {"processed_file_path": processed_file_path,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
         
 
-@app.post("/damage-detection2/")
-async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
-    dir_name="s3_files/"
-    s3_url=body.get("s3_url")
-    extension=body.get("extension")
-    file_name=random_name_generator()
+# @app.post("/damage-detection2/")
+# async def damage_detection(body: dict = Body(...), api_token:str=Depends(get_api_token)):
+#     dir_name="s3_files/"
+#     s3_url=body.get("s3_url")
+#     extension=body.get("extension")
+#     file_name=random_name_generator()
 
-    if s3_file_downloader(s3_url,file_name+extension):
-        processed_file_path=None
-        if extension.lower()==".jpg":
-            processed_file_path,message,damage_rectangle,original_img_info=damage_detection_in_image2(dir_name,file_name,extension)
-            if processed_file_path!=None:
-                uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-            return {"image_s3_link":s3_url,"processed_img_s3_link":uploaded_s3_link,"extension":extension,"message":message,"org_img_info":original_img_info,"damages":damage_rectangle}
+#     if s3_file_downloader(s3_url,file_name+extension):
+#         processed_file_path=None
+#         if extension.lower()==".jpg":
+#             processed_file_path,message,damage_rectangle,original_img_info=damage_detection_in_image2(dir_name,file_name,extension)
+#             if processed_file_path!=None:
+#                 uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+#             return {"image_s3_link":s3_url,"processed_img_s3_link":uploaded_s3_link,"extension":extension,"message":message,"org_img_info":original_img_info,"damages":damage_rectangle}
         
-        elif extension.lower()==".mp4":
-            processed_file_path,message=damage_detection_in_video2(dir_name, file_name ,extension)
+#         elif extension.lower()==".mp4":
+#             processed_file_path,message=damage_detection_in_video2(dir_name, file_name ,extension)
     
-            if processed_file_path!=None:
-                uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
-            return {"processed_file_path":file_name+extension,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
+#             if processed_file_path!=None:
+#                 uploaded_s3_link=upload_file_to_s3_bucket(processed_file_path,file_name,extension)
+#             return {"processed_file_path":file_name+extension,"uploaded_s3_link":uploaded_s3_link,"extension":extension,"message":message}
         
 @app.post("/llm-chat/")
 async def llm_chat_response(chat_request: ChatRequest, api_token:str=Depends(get_api_token)):
@@ -110,10 +110,15 @@ async def llm_chat_response(chat_request: ChatRequest, api_token:str=Depends(get
             {
             "role": "user",
             "content": [
+                # {
+                # "type": "text",
+                # "text": "I have provided "+chat_request.image_side+" image of the vehicle. Please check the small spots, small dents and big dents on the vehicle body and also suggest the estimated repare cost based on detected damages in dollar and also add one line comment, if any damage detected return in the following json format\
+                #     {'small_dents':2/3 , 'small_spots':3/10,'big_dents':0/4 or any detected big dents, 'scratches':1 or 6 or any detected scratches,'cost':50/200, 'comment':'small dents and spots detected but there is no severe damage detected.'}, and if no the simply return {'small_dents':0, 'small_spots':0,'big_dents':0,'scratches':0,'cost':0, 'comment':'There is no damage found.'} and no cost. Please make sure the response should be 100 persent in the json format any there is no any text defore and end.",
+                # },
                 {
                 "type": "text",
-                "text": "I have provided "+chat_request.image_side+" image of the vehicle. Please check the small spots, small dents and big dents on the vehicle body and also suggest the estimated repare cost based on detected damages in dollar and also add one line comment, if any damage detected return in the following json format\
-                    {'small_dents':2/3 , 'small_spots':3/10,'big_dents':0/4 or any detected big dents, 'scratches':1 or 6 or any detected scratches,'cost':50/200, 'comment':'small dents and spots detected but there is no severe damage detected.'}, and if no the simply return {'small_dents':0, 'small_spots':0,'big_dents':0,'scratches':0,'cost':0, 'comment':'There is no damage found.'} and no cost. Please make sure the response should be 100 persent in the json format any there is no any text defore and end.",
+                "text": "I have provided "+chat_request.image_side+" image of the vehicle. Please deeply inspect the whole vehicle if you find any spots, scratches, dents, or any damage and alos focus on small scratches, dent and damages, please write in the comment with locations only 7-8 words and also suggest the expected cost for fixing vehicle and response return in the following json format\
+                    {'cost':50/200, 'comment':'2 scratched and 1 dent on the right door./ 4 spots on front door and scratch on window.'}, and if no the simply return {'cost':0, 'comment':'There is no damage found.'} and no cost. Please make sure the response should be 100 percent in the json format any there is no any text before and after/end.",
                 },
                 {
                 "type": "image_url",
@@ -143,8 +148,8 @@ async def llm_chat_response(chat_request: ChatRequest, api_token:str=Depends(get
             "content": [
                 {
                 "type": "text",
-                "text": "I have provided two"+chat_request.image_side+" images of the same vehicle. I want to make comparison of first image with my second new image I want to check if there is any new small spots, small dents and big dents on the vehicle body and also suggest the estimated repare cost based on detected damages in dollar and also add one line comment, if any new damage detected return in the following json format\
-                    {'small_dents':2/3 , 'small_spots':3/10,'big_dents':0/4 or any detected big dents, 'scratches':1 or 6 or any detected scratches,'cost':50/200, 'comment':'small dents and spots detected but there is no severe damage detected.'}, and if no the simply return {'small_dents':0, 'small_spots':0,'big_dents':0,'scratches':0,'cost':0, 'comment':'There is no new damage found.'} and no cost. Please make sure the response should be 100 persent in the json format any there is no any text defore and end.",
+                "text": "I have provided two "+chat_request.image_side+" image of the same vehicle. One picture is from previously inspected and second image is fresh picture Please deeply inspect the whole two vehicles if you find any NEW spots, scratches, dents, or any damage and also focus on NEW small scratches, dent and damages, please write in the comment with locations only 7-8 words and also suggest the expected cost for fixing vehicle and response return in the following json format\
+                    {'cost':50/200, 'comment':'2 new scratches and 1 new dent on the right door detected./ 4 snew pots on front door and new scratch on window detected.'}, and if no the simply return {'cost':0, 'comment':'There is no new damage found.'} and no cost. Please make sure the response should be 100 percent in the json format any there is no any text before and after/end.",
                 },
                 {
                 "type": "image_url",
