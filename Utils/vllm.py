47d73e0f27a6bb7e24d830cc944feb527c514421
Ali Ahmad <Alijakhar129@gmail.com>
2024-11-12 16:04:05 +0500

    fix json response issue

--- Utils/vllm.py
+++ Utils/vllm.py
@@ -2,6 +2,7 @@ import os
 from mistralai import Mistral
 from dotenv.main import load_dotenv
 import json
+import re
 load_dotenv()
 
 model = os.environ['MISTRALAI_MODEL']
@@ -10,7 +11,14 @@ mistral_client = Mistral(api_key=os.environ['MISTRALAI_KEY'])
 def parse_json_response(input_data):
     try:
         if isinstance(input_data, str):
-            input_data = input_data.strip("```json").strip("```").strip()
+            # Use regex to find the JSON structure between curly braces
+            json_match = re.search(r"\{[\s\S]*\}", input_data)
+            if json_match:
+                # Extract the JSON portion
+                input_data = json_match.group(0)
+            else:
+                return {"error": "No JSON found in input data"}
+            
             parsed_data = json.loads(input_data)
         elif isinstance(input_data, dict):
             parsed_data = input_data
@@ -20,6 +28,8 @@ def parse_json_response(input_data):
         return parsed_data
     except json.JSONDecodeError:
         return {"error": "Invalid JSON format"}
+
+
 
 
 
@@ -43,7 +53,9 @@ def get_mistral_analysis(s3_url):
     ]
     try:
         chat_response = mistral_client.chat.complete(model=model, messages=messages)
+        # print("Chat response vllm: ", chat_response.choices[0].message.content)
         parsed_respone=parse_json_response(chat_response.choices[0].message.content)
+        # print("Chat response after parsing: ", parsed_respone)
         return parsed_respone    
     except Exception as e:
         return f"Error getting Mistral analysis: {str(e)}"
