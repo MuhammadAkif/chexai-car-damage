e214cf61027cb0518eaa49db3ba58354bbe3bd56
romandev0225 <rrudenkorroman@gmail.com>
2025-04-23 09:12:34 +0500

    add previous endpoints

--- Utils/night_image_captured.py
+++ Utils/night_image_captured.py
@@ -9,7 +9,7 @@ import cv2
 import re
 
 
-def is_night_captured_image(image_url,threshold=40):
+def is_night_captured_image(image_url,threshold=20):
     try:
         response = requests.get(image_url)
     except:
